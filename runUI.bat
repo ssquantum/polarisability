de11be4a3b9ed7c3af977fde064550da53a972ef
SPENCE <qgtx64@durham.ac.uk>
2019-11-23 08:26:34 +0000

    generalise python file path

--- runUI.bat
+++ runUI.bat
@@ -1,2 +1,2 @@
 rem -- This file initiates the AtomFieldInt_V3 user interface using the local enthought python
-C:\Users\Lab\AppData\Local\Enthought\Canopy\edm\envs\User\python.exe AtomFieldInt_V3.py rungui
+%LOCALAPPDATA%\Enthought\Canopy\edm\envs\User\python.exe AtomFieldInt_V3.py rungui
